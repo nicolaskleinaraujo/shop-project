85636c54318151a4cf418f7e494a1eb60e37faae
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-01 13:54:47 -0300

    configured cors and json on the server

---
+++ backend/src/index.js
@@ -0,0 +1,23 @@
+// Imports
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const port = process.env.PORT || 3000
+
+// Configs
+app.use(express.json())
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+)
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("Bem-vindo a minha API, consulte o projeto para mais informações!")
+})
+
+app.listen(port, () => {
+  console.log("Server running!")
+})
