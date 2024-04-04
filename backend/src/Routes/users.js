95528054b66983b6f5c7c5a6341bb933d3fb5d80
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-04 15:26:18 -0300

    added the update function on the router

--- backend/src/Routes/users.js
+++ backend/src/Routes/users.js
@@ -5,5 +5,6 @@ const userController = require("../controllers/userController")
 // Routes
 router.route("/user/create").post((req, res) => userController.create(req, res))
 router.route("/user/:id").get((req, res) => userController.getById(req, res))
+router.route("/user/update").post((req, res) => userController.update(req, res))
 
 module.exports = router
