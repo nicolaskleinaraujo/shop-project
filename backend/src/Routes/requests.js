2993c18b0043f2dfd5ee5e3c9819f19c2e8856eb
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-04 23:22:15 -0300

    hotfixed the router

--- backend/src/Routes/requests.js
+++ backend/src/Routes/requests.js
@@ -6,6 +6,6 @@ const requestController = require("../controllers/requestController")
 router.route("/request/create").post((req, res) => requestController.create(req, res))
 router.route("/request/:id").get((req, res) => requestController.getById(req, res))
 router.route("/request/update").post((req, res) => requestController.update(req, res))
-router.route("request/:id").delete((req, res) => requestController.delete(req, res))
+router.route("/request/:id").delete((req, res) => requestController.delete(req, res))
 
 module.exports = router
