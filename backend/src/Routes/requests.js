81f747cb8ee80897f767d52ec1cc3cc62e9987e6
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-04 22:47:23 -0300

    added the get by id function on the router

--- backend/src/Routes/requests.js
+++ backend/src/Routes/requests.js
@@ -4,5 +4,6 @@ const requestController = require("../controllers/requestController")
 
 // Routes
 router.route("/request/create").post((req, res) => requestController.create(req, res))
+router.route("/request/:id").get((req, res) => requestController.getById(req, res))
 
 module.exports = router
