098bba5e659f89749e920392f515f2ca275dd9e1
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-05 14:56:45 -0300

    added the login function on the router

--- backend/src/Routes/users.js
+++ backend/src/Routes/users.js
@@ -7,5 +7,6 @@ router.route("/user/create").post((req, res) => userController.create(req, res))
 router.route("/user/:id").get((req, res) => userController.getById(req, res))
 router.route("/user/update").post((req, res) => userController.update(req, res))
 router.route("/user/:id").delete((req, res) => userController.delete(req, res))
+router.route("/user/login").post((req, res) => userController.login(req, res))
 
 module.exports = router
