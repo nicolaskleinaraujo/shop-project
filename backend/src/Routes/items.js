04cc5bfe379289f5056347817669a53a5e606451
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-06 12:17:47 -0300

    added the create item function on the item router

--- backend/src/Routes/items.js
+++ backend/src/Routes/items.js
@@ -3,5 +3,6 @@ const router = require("express").Router()
 const itemController = require("../controllers/itemController")
 
 // Routes
+router.route("/item/create").post((req, res) => itemController.create(req, res))
 
 module.exports = router
