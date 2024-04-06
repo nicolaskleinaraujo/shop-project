1bfa67fdafee37bd16ba44e84b226a3bb836af49
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-06 14:24:48 -0300

    added the update item function on the router

--- backend/src/Routes/items.js
+++ backend/src/Routes/items.js
@@ -5,5 +5,6 @@ const itemController = require("../controllers/itemController")
 // Routes
 router.route("/item/create").post((req, res) => itemController.create(req, res))
 router.route("/item/:id").get((req, res) => itemController.getById(req, res))
+router.route("/item/update").post((req, res) => itemController.update(req, res))
 
 module.exports = router
