b9b17f6474683ea537071e90d3cd80b858650b33
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-06 14:31:58 -0300

    added the delete item on the router

--- backend/src/Routes/items.js
+++ backend/src/Routes/items.js
@@ -6,5 +6,6 @@ const itemController = require("../controllers/itemController")
 router.route("/item/create").post((req, res) => itemController.create(req, res))
 router.route("/item/:id").get((req, res) => itemController.getById(req, res))
 router.route("/item/update").post((req, res) => itemController.update(req, res))
+router.route("/item/:id").delete((req, res) => itemController.delete(req, res))
 
 module.exports = router
