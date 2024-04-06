fa8122bbb836e78bbc7dabef3065475dea48f7e0
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-06 12:15:22 -0300

    created create item function on item controller

--- backend/src/controllers/itemController.js
+++ backend/src/controllers/itemController.js
@@ -2,13 +2,35 @@
 const prisma = require("../db/client")
 
 const itemController = {
-    create: async (req, res) => {
-        const name = req.body.name
-        const description = req.body.description
-        const value = parseInt(req.body.value)
+  create: async (req, res) => {
+    const name = req.body.name
+    const description = req.body.description
+    const value = parseInt(req.body.value)
 
-        
+    if (
+      name === "" ||
+      name === undefined ||
+      description === "" ||
+      description === undefined ||
+      isNaN(value)
+    ) {
+      res.status(400).json({ msg: "Informações insuficientes" })
+      return
     }
+
+    try {
+      prisma.item.create({
+        data: {
+          name,
+          description,
+          value,
+        },
+      })
+      res.status(200).json({ msg: `${name} criado com sucesso` })
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  },
 }
 
 module.exports = itemController
