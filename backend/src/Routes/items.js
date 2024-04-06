93d54ea651231f09780cb048c8748c65855fe07b
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-06 14:18:00 -0300

    added the get item by id function on the router

--- backend/src/Routes/items.js
+++ backend/src/Routes/items.js
@@ -4,5 +4,6 @@ const itemController = require("../controllers/itemController")
 
 // Routes
 router.route("/item/create").post((req, res) => itemController.create(req, res))
+router.route("/item/:id").get((req, res) => itemController.getById(req, res))
 
 module.exports = router
