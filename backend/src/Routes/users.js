562723a6614cf3987006d7b97c6f5b5f7d4cf330
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-08 19:32:13 -0300

    hotfix

--- backend/src/Routes/users.js
+++ backend/src/Routes/users.js
@@ -4,10 +4,10 @@ const userController = require("../controllers/userController")
 
 // Routes
 router.route("/user/create").post((req, res) => userController.create(req, res))
+router.route("/user/tryauth").get((req, res) => userController.tryAuth(req, res))
 router.route("/user/:id").get((req, res) => userController.getById(req, res))
 router.route("/user/update").post((req, res) => userController.update(req, res))
 router.route("/user/:id").delete((req, res) => userController.delete(req, res))
 router.route("/user/login").post((req, res) => userController.login(req, res))
-router.route("/user/tryauth").get((req, res) => userController.tryAuth(req, res))
 
 module.exports = router
