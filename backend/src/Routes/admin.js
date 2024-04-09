eb6536acf478b90ccf95242b1869eb79607d5c12
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-09 15:13:13 -0300

    added the adminAuth to the admin router

--- backend/src/Routes/admin.js
+++ backend/src/Routes/admin.js
@@ -1,8 +1,9 @@
 // Imports
 const router = require("express").Router()
 const adminController = require("../controllers/adminController")
+const adminAuth = require("../middlewares/adminAuth")
 
 // Routes
-router.route("/admin/requests").get((req, res) => adminController.getRequests(req, res))
+router.route("/admin/requests").get(adminAuth, (req, res) => adminController.getRequests(req, res))
 
 module.exports = router
