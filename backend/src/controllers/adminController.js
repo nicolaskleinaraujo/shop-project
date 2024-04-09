d4b6c96c2788e9630b0d2639c9ef08a0d33516a1
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-09 15:24:34 -0300

    created the getUsers function

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -10,6 +10,15 @@ const adminController = {
       res.status(500).json(err)
     }
   },
+
+  getUsers: async (req, res) => {
+    try {
+      const users = await prisma.user.findMany()
+      res.status(200).json(users)
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  },
 }
 
 module.exports = adminController
