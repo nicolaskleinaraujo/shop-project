c06ba237c3aaf0391c22d9ca4ddb9d8a3e546c47
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-09 13:17:22 -0300

    created the cookieAuth middleware

---
+++ backend/src/middlewares/cookieAuth.js
@@ -0,0 +1,38 @@
+// Imports
+const prisma = require("../db/client")
+const jwt = require("jsonwebtoken")
+
+async function cookieAuth(req, res, next) {
+    const jwtCookie = req.signedCookies.jwt
+
+    if (!jwtCookie) {
+        res.status(400).json({ msg: "Não possui cookie jwt" })
+        return
+    }
+
+    try {
+        jwt.verify(jwtCookie, process.env.JWT_SECRET, (err) => {
+            if (err) {
+                res.status(400).json({ msg: "Cookie não autenticado" })
+                return
+            }
+        })
+
+        const user = await prisma.user.findUnique({
+            where: {
+                jwt: jwtCookie
+            }
+        })
+
+        if (!user) {
+            res.status(400).json({ msg: "Usuario não identificado" })
+            return
+        }
+
+        next()
+    } catch (err) {
+        res.status(500).json(err)
+    }
+}
+
+module.exports = cookieAuth
