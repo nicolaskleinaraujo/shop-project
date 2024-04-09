941fc69f523626e201ae2422892010f45678206c
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-09 14:27:07 -0300

    added the cookie auth on the delete user route

--- backend/src/Routes/users.js
+++ backend/src/Routes/users.js
@@ -8,7 +8,7 @@ router.route("/user/create").post((req, res) => userController.create(req, res))
 router.route("/user/tryauth").get((req, res) => userController.tryAuth(req, res))
 router.route("/user/:id").get((req, res) => userController.getById(req, res))
 router.route("/user/update").post(cookieAuth, (req, res) => userController.update(req, res))
-router.route("/user/:id").delete((req, res) => userController.delete(req, res))
+router.route("/user/:id").delete(cookieAuth, (req, res) => userController.delete(req, res))
 router.route("/user/login").post((req, res) => userController.login(req, res))
 
 module.exports = router
