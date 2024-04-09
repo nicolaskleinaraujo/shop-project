7364421be0b9e6446c82ab823ec414758f64988b
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-09 15:02:51 -0300

    created the get all requests function

--- backend/src/controllers/adminController.js
+++ backend/src/controllers/adminController.js
@@ -5,6 +5,7 @@ const adminController = {
   getRequests: async (req, res) => {
     try {
       const requests = await prisma.request.findMany()
+      res.status(200).json(requests)
     } catch (err) {
       res.status(500).json(err)
     }
