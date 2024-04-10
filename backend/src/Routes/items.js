13e91612d0173f63839294e4d372ef4ebe9292b5
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-10 11:01:08 -0300

    added adminAuth middleware on the item router

--- backend/src/Routes/items.js
+++ backend/src/Routes/items.js
@@ -1,11 +1,12 @@
 // Imports
 const router = require("express").Router()
 const itemController = require("../controllers/itemController")
+const adminAuth = require("../middlewares/adminAuth")
 
 // Routes
-router.route("/item/create").post((req, res) => itemController.create(req, res))
-router.route("/item/:id").get((req, res) => itemController.getById(req, res))
-router.route("/item/update").post((req, res) => itemController.update(req, res))
-router.route("/item/:id").delete((req, res) => itemController.delete(req, res))
+router.route("/item/create").post(adminAuth, (req, res) => itemController.create(req, res))
+router.route("/item/:id").get(adminAuth, (req, res) => itemController.getById(req, res))
+router.route("/item/update").post(adminAuth, (req, res) => itemController.update(req, res))
+router.route("/item/:id").delete(adminAuth, (req, res) => itemController.delete(req, res))
 
 module.exports = router
