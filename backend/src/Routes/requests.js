3c934d4a0a3b7ce849edfaea3b6426b0662ad685
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-10 13:47:22 -0300

    hotfixed the requests router

--- backend/src/Routes/requests.js
+++ backend/src/Routes/requests.js
@@ -3,12 +3,13 @@ const router = require("express").Router()
 const requestController = require("../controllers/requestController")
 const cookieAuth = require("../middlewares/cookieAuth")
 const authorAuth = require("../middlewares/authorAuth")
+const adminAuth = require("../middlewares/adminAuth")
 
 // Routes
 router.route("/request/create").post(cookieAuth, (req, res) => requestController.create(req, res))
 router.route("/request/:id").get(cookieAuth, (req, res) => requestController.getById(req, res))
 router.route("/request/update").post(authorAuth, (req, res) => requestController.update(req, res))
 router.route("/request/:id").delete(authorAuth, (req, res) => requestController.delete(req, res))
-router.route("/request/delivery").post(authorAuth, (req, res) => requestController.setDelivered(req, res))
+router.route("/request/:id").post(adminAuth, (req, res) => requestController.setDelivered(req, res))
 
 module.exports = router
