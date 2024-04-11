3705ff416857b14591eaf6c76d7d9162f50d8953
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-11 17:44:34 -0300

    hotfixed the request router

--- backend/src/Routes/requests.js
+++ backend/src/Routes/requests.js
@@ -7,12 +7,12 @@ const adminAuth = require("../middlewares/adminAuth")
 
 // Routes
 router.route("/request/create").post(cookieAuth, (req, res) => requestController.create(req, res))
-router.route("/request/user").get(cookieAuth, (req, res) => requestController.getByUser(req, res))
-router.route("/request/:slug").get(cookieAuth, (req, res) => requestController.getBySlug(req, res))
-router.route("/request/:id").get(cookieAuth, (req, res) => requestController.getById(req, res))
+router.route("/request/id/:id").get(cookieAuth, (req, res) => requestController.getById(req, res))
 router.route("/request/update").post(authorAuth, (req, res) => requestController.update(req, res))
-router.route("/request/:id").delete(authorAuth, (req, res) => requestController.delete(req, res))
+router.route("/request/delete/:id").delete(authorAuth, (req, res) => requestController.delete(req, res))
+router.route("/request/user").get(cookieAuth, (req, res) => requestController.getByUser(req, res))
+router.route("/request/slug/:slug").get(cookieAuth, (req, res) => requestController.getBySlug(req, res))
 router.route("/request").get(adminAuth, (req, res) => requestController.getRequests(req, res))
-router.route("/request/:id").post(adminAuth, (req, res) => requestController.setDelivered(req, res))
+router.route("/request/delivered/:id").post(adminAuth, (req, res) => requestController.setDelivered(req, res))
 
 module.exports = router
