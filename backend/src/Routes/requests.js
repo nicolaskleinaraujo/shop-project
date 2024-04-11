18e04a8773ac04a661b8f455d41e7a0c65fb7f16
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-11 17:36:44 -0300

    added the get request by slug on the router

--- backend/src/Routes/requests.js
+++ backend/src/Routes/requests.js
@@ -8,6 +8,7 @@ const adminAuth = require("../middlewares/adminAuth")
 // Routes
 router.route("/request/create").post(cookieAuth, (req, res) => requestController.create(req, res))
 router.route("/request/user").get(cookieAuth, (req, res) => requestController.getByUser(req, res))
+router.route("/request/:slug").get(cookieAuth, (req, res) => requestController.getBySlug(req, res))
 router.route("/request/:id").get(cookieAuth, (req, res) => requestController.getById(req, res))
 router.route("/request/update").post(authorAuth, (req, res) => requestController.update(req, res))
 router.route("/request/:id").delete(authorAuth, (req, res) => requestController.delete(req, res))
