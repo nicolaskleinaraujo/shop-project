51800d46a0ef76585d3ff3a8ceb8cca1b0292c51
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-12 22:15:12 -0300

    prettier

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -6,40 +6,39 @@ import dbFetch from "../../axios/config"
 import { useEffect, useState } from "react"
 
 const Home = () => {
-    const [items, setItems] = useState([])
+  const [items, setItems] = useState([])
 
-    const getItems = async() => {
-        const res = await dbFetch.get("/item/items")
-        setItems(res.data)
+  const getItems = async () => {
+    const res = await dbFetch.get("/item/items")
+    setItems(res.data)
+  }
+
+  const addToCart = (productName) => {
+    const cart = localStorage.getItem("cart")
+    if (!cart) {
+      localStorage.setItem("cart", productName)
+    } else if (cart) {
+      localStorage.setItem("cart", `${cart}, ${productName}`)
     }
+  }
 
-    const addToCart = (productName) => {
-        const cart = localStorage.getItem("cart")
-        if (!cart) {
-            localStorage.setItem("cart", productName)
-        } else if (cart) {
-            localStorage.setItem("cart", `${cart}, ${productName}`)
-        }
-    }
+  useEffect(() => {
+    getItems()
+  }, [])
 
-    useEffect(() => {
-        getItems()
-    }, [])
-
-    return (
-        <div className={styles.home}>
-            {items && (
-                items.map((item) => (
-                    <div key={item.id}>
-                        <p className={styles.name}>{item.name}</p>
-                        <p className={styles.value}>R$ {item.value}</p>
-                        <p className={styles.description}>{item.description}</p>
-                        <button onClick={() => addToCart(item.name)}>Adicionar ao carrinho</button>
-                    </div>
-                ))
-            )}
-        </div>
-    )
+  return (
+    <div className={styles.home}>
+      {items &&
+        items.map((item) => (
+          <div key={item.id}>
+            <p className={styles.name}>{item.name}</p>
+            <p className={styles.value}>R$ {item.value}</p>
+            <p className={styles.description}>{item.description}</p>
+            <button onClick={() => addToCart(item.name)}>Adicionar ao carrinho</button>
+          </div>
+        ))}
+    </div>
+  )
 }
 
 export default Home
