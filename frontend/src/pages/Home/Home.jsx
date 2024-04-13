459159e11165c2bff1a383340717c470545b036b
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-12 21:23:11 -0300

    created the getItems function on the home page

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,27 @@
 // CSS
 import styles from "./Home.module.css"
 
+// Modules
+import dbFetch from "../../axios/config"
+import { useEffect, useState } from "react"
+
 const Home = () => {
-  return (
-    <div>
-        <h1>Home</h1>
-    </div>
-  )
+    const [items, setItems] = useState([])
+
+    const getItems = async() => {
+        const res = await dbFetch.get("/item/items")
+        setItems(res.data)
+    }
+
+    useEffect(() => {
+        getItems()
+    }, [])
+
+    return (
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
 }
 
 export default Home
