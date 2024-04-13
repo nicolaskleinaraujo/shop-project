d91401d63f340fbd41e9ab6b184741f3b4212132
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-13 16:02:04 -0300

    hotfixed the getItems function

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -6,47 +6,43 @@ import dbFetch from "../../axios/config"
 import { useState, useEffect } from "react"
 
 const Cart = () => {
-    const [items, setItems] = useState([])
-    const [values, setValues] = useState([])
+  const [items, setItems] = useState([])
+  const [values, setValues] = useState([])
 
-    const getItems = () => {
-        const storedCart = localStorage.getItem("cart")
-        const itemsId = storedCart.split(", ")
+  const getItems = async() => {
+    const storedCart = localStorage.getItem("cart")
+    const itemsId = storedCart.split(", ")
 
-        const itemsArray = []
-        const valuesArray = []
+    const promises = itemsId.map(async(itemId) => {
+      const res = await dbFetch.get(`/item/${itemId}`)
+      return res.data
+    })
+    const results = await Promise.all(promises)
 
-        if (itemsId) {
-            itemsId.forEach(async(itemId) => {
-                const res = await dbFetch.get(`/item/${itemId}`)
-                itemsArray.push(res.data.name)
-                valuesArray.push(res.data.value)
-            })
-        }
+    const itemsArray = results.map((result) => result.name)
+    const valuesArray = results.map((result) => result.value)
 
-        setItems(itemsArray)
-        setValues(valuesArray)
-    }
+    setItems(itemsArray)
+    setValues(valuesArray)
+  }
 
-    useEffect(() => {
-        getItems()
-        console.log("testeuseeffect")
-    }, [])
+  useEffect(() => {
+    getItems()
+  }, [])
 
-    return (
-        <div>
-            <h1>Cart</h1>
-            <button onClick={() => console.log(items)}>teste</button>
-            <button onClick={() => console.log(values)}>teste2</button>
-            <div>
-                {items && items.map((item, index) => {
-                    <div key={index}>
-                        {item} | {values[index]}
-                    </div>
-                })}
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Cart</h1>
+
+      {items &&
+        items.map((item, index) => (
+          <div key={index}>
+            {item} | R$ {values[index]}
+          </div>
+        ))
+      }
+    </div>
+  )
 }
 
 export default Cart
