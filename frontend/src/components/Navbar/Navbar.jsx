37fa82b0f8b262eac4c7e451f99c3551581b0058
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-12 22:44:48 -0300

    added link to the navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -9,7 +9,10 @@ const Navbar = () => {
         <nav className={styles.navbar}>
             <div className={styles.search}>PESQ</div>
             <div><Link to="/"><img src="https://via.placeholder.com/50x50" alt="Logo do Site" /></Link></div>
-            <div className={styles.menu}>MENU</div>
+            <div className={styles.menu}>
+                <Link to="/">CA</Link> 
+                <Link to="/">US</Link>
+            </div>
         </nav>
     )
 }
