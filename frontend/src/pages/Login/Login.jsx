89046dc044259a3f086e770b76ef75997e5f8a37
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-13 18:13:03 -0300

    created the login form

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,12 +1,41 @@
 // CSS
 import styles from "./Login.module.css"
 
+// Modules
+import dbFetch from "../../axios/config"
+import { useState } from "react"
+
 const Login = () => {
-  return (
-    <div>
-        <h1>Login</h1>
-    </div>
-  )
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleLogin = async () => {
+        
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleLogin}>
+                <h1>Login</h1>
+
+                <input 
+                    type="email" 
+                    placeholder="Email" 
+                    onChange={(e) => setEmail(e.target.value)} 
+                    value={email} 
+                />
+
+                <input 
+                    type="password"  
+                    placeholder="Senha" 
+                    onChange={(e) => setPassword(e.target.value)} 
+                    value={password}
+                />
+
+                <input type="submit" value="Logar" />
+            </form>
+        </div>
+    )
 }
 
 export default Login
