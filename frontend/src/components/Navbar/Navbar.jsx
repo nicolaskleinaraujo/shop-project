13d94d354ca87b7664d72ad6f0a6a139013ed532
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-12 22:30:15 -0300

    created the navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,9 +2,13 @@
 import styles from "./Navbar.module.css"
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.search}>PESQ</div>
+            <div><img src="https://via.placeholder.com/50x50" alt="Logo do Site" /></div>
+            <div className={styles.menu}>MENU</div>
+        </nav>
+    )
 }
 
 export default Navbar
