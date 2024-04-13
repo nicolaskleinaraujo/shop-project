c38dbd590e02a91204ca0c198f6ecffd2a57d229
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-12 22:34:10 -0300

    added the link to the img on the navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,14 @@
 // CSS
 import styles from "./Navbar.module.css"
 
+// Modules
+import { Link } from "react-router-dom"
+
 const Navbar = () => {
     return (
         <nav className={styles.navbar}>
             <div className={styles.search}>PESQ</div>
-            <div><img src="https://via.placeholder.com/50x50" alt="Logo do Site" /></div>
+            <div><Link to="/"><img src="https://via.placeholder.com/50x50" alt="Logo do Site" /></Link></div>
             <div className={styles.menu}>MENU</div>
         </nav>
     )
