36f58570d8d4af49c5a6f4dda79a34223cc6849b
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-13 18:20:27 -0300

    created the login function

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -4,38 +4,51 @@ import styles from "./Login.module.css"
 // Modules
 import dbFetch from "../../axios/config"
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const Login = () => {
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
+  const navigate = useNavigate()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
-    const handleLogin = async () => {
-        
+  const handleLogin = async (e) => {
+    e.preventDefault()
+
+    try {
+      const res = await dbFetch.post("/user/login", {
+        email,
+        password,
+      })
+
+      navigate("/")
+    } catch (err) {
+      console.log(err)
     }
+  }
 
-    return (
-        <div>
-            <form onSubmit={handleLogin}>
-                <h1>Login</h1>
+  return (
+    <div>
+      <form onSubmit={handleLogin}>
+        <h1>Login</h1>
 
-                <input 
-                    type="email" 
-                    placeholder="Email" 
-                    onChange={(e) => setEmail(e.target.value)} 
-                    value={email} 
-                />
+        <input
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
 
-                <input 
-                    type="password"  
-                    placeholder="Senha" 
-                    onChange={(e) => setPassword(e.target.value)} 
-                    value={password}
-                />
+        <input
+          type="password"
+          placeholder="Senha"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
 
-                <input type="submit" value="Logar" />
-            </form>
-        </div>
-    )
+        <input type="submit" value="Logar" />
+      </form>
+    </div>
+  )
 }
 
 export default Login
