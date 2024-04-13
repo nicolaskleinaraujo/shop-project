bc0276d1b7d15a26d953477823ed63e95aa4aa3e
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-13 12:01:24 -0300

    created the getItems function to the cart page

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -1,12 +1,43 @@
 // CSS
 import styles from "./Cart.module.css"
 
+// Modules
+import dbFetch from "../../axios/config"
+import { useState, useEffect } from "react"
+
 const Cart = () => {
-  return (
-    <div>
-        <h1>Cart</h1>
-    </div>
-  )
+    const [cart, setCart] = useState("")
+    const [items, setItems] = useState([])
+    const [prices, setPrices] = useState([])
+
+    const getItems = () => {
+        setCart(localStorage.getItem("cart"))
+        setItems(cart.split(", "))
+    }
+
+    const getPrice = async(itemName) => {
+        await dbFetch()
+    }
+
+    useEffect(() => {
+        getItems()
+    }, [])
+
+    return (
+        <div>
+            <h1>Cart</h1>
+            <button onClick={() => console.log(items)}>teste</button>
+            <button onClick={() => console.log(cart)}>teste2</button>
+            <div>
+                {items && (
+                    items.map((item) => (
+                        <div>{item} || {getPrice(item)}</div>
+                    ))
+                )}
+            </div>
+            
+        </div>
+    )
 }
 
 export default Cart
