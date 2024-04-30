acfea2e58dfac7bedea05b3bda1534a4f4e9bf59
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-29 22:08:09 -0300

    added the tryAuth function to the app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
 // CSS
 import './App.css'
 
+// Modules
+import dbFetch from './axios/config'
+import { useEffect } from 'react'
+
 // Router
 import Router from './utils/Router'
 
 function App() {
+  const tryAuth = async() => {
+    await dbFetch.get("/user/tryauth")
+  }
+
+  useEffect(() => {
+    tryAuth()
+  }, [])
+
   return (
     <>
       <Router />
