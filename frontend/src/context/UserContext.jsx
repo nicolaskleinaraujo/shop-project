c798dd9b9cee6f41f0ee3d888756d529cea3ba76
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-30 20:02:28 -0300

    created the user context

---
+++ frontend/src/context/UserContext.jsx
@@ -0,0 +1,16 @@
+// Modules
+import { Children, createContext, useState } from "react"
+
+// Context
+export const UserContext = createContext()
+
+// Provider
+export const UserProvider = ({ children }) => {
+    const [userId, setUserId] = useState(0)
+
+    return (
+        <UserContext.Provider value={{ userId, setUserId }}>
+            {children}
+        </UserContext.Provider>
+    )
+}
