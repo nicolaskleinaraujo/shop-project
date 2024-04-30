9bdc8394e264749baf36d7339b971d1b58d3dac3
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-30 20:03:54 -0300

    added the user context to the project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Context
+import { UserProvider } from './context/UserContext.jsx'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <App />
+    </UserProvider>
   </React.StrictMode>,
 )
