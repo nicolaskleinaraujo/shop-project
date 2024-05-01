927cdd0fe553cdfcfb91a484f8dfb12ab13cb642
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-01 13:27:54 -0300

    created the request by user page

---
+++ frontend/src/pages/RequestByUser/RequestByUser.jsx
@@ -0,0 +1,12 @@
+// CSS
+import styles from "./RequestByUser.module.css"
+
+const RequestByUser = () => {
+  return (
+    <div>
+        <h1>RequestByUser</h1>
+    </div>
+  )
+}
+
+export default RequestByUser
