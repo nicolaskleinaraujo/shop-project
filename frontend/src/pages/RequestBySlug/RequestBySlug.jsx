94f2fb58d33f96573bd5ec2955564a7d38ec9482
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-01 13:45:53 -0300

    created the request by slug page

---
+++ frontend/src/pages/RequestBySlug/RequestBySlug.jsx
@@ -0,0 +1,12 @@
+// CSS
+import styles from "./RequestBySlug.module.css"
+
+const RequestBySlug = () => {
+  return (
+    <div>
+        <h1>Request By Slug</h1>
+    </div>
+  )
+}
+
+export default RequestBySlug
