a1eae6ea672dd808da2230af6997cca136331c1b
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-01 13:50:05 -0300

    added the getRequest function

--- frontend/src/pages/RequestBySlug/RequestBySlug.jsx
+++ frontend/src/pages/RequestBySlug/RequestBySlug.jsx
@@ -1,12 +1,30 @@
 // CSS
 import styles from "./RequestBySlug.module.css"
 
+// Modules
+import dbFetch from "../../axios/config"
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
 const RequestBySlug = () => {
-  return (
-    <div>
-        <h1>Request By Slug</h1>
-    </div>
-  )
+    const { slug } = useParams()
+
+    const [request, setRequest] = useState([])
+    const getRequest = async() => {
+        const res = await dbFetch.get(`/request/slug/${slug}`)
+        setRequest(res.data)
+    }
+
+    useEffect(() => {
+        getRequest()
+    }, [])
+
+    return (
+        <div>
+            <h1>Request By Slug</h1>
+            <button onClick={() => console.log(request)}>teste</button>
+        </div>
+    )
 }
 
 export default RequestBySlug
