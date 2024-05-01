35ee6dac0b47c701d823267e9f28df04101a05c2
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-01 13:23:13 -0300

    created the footer component

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+// CSS
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>&copy; 2024 - Site desenvolvido por <a href="https://github.com/nicolaskleinaraujo" target="_blank">Nicolas Klein</a>.</p>
+        <p>Favor, utilizar as features com moderação e consciência.</p>
+    </footer>
+  )
+}
+
+export default Footer
