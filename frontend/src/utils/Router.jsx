f9a24aa477d8a540bff0010699a054c803786ef1
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-01 13:28:40 -0300

    added the request by user on the router

--- frontend/src/utils/Router.jsx
+++ frontend/src/utils/Router.jsx
@@ -10,6 +10,7 @@ import Home from "../pages/Home/Home"
 import Cart from "../pages/Cart/Cart"
 import Login from "../pages/Login/Login"
 import Register from "../pages/Register/Register"
+import RequestByUser from "../pages/RequestByUser/RequestByUser"
 
 const Router = () => {
   return (
@@ -21,6 +22,7 @@ const Router = () => {
                 <Route path="/my-cart" element={<Cart />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/requests" element={<RequestByUser />} />
             </Routes>
             <Footer />
         </BrowserRouter>
