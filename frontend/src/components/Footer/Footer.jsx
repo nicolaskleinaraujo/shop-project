9867ba0ed3f73c8a1ca7e789169952a537022498
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-01 13:25:29 -0300

    added css to the footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css"
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
         <p>&copy; 2024 - Site desenvolvido por <a href="https://github.com/nicolaskleinaraujo" target="_blank">Nicolas Klein</a>.</p>
         <p>Favor, utilizar as features com moderação e consciência.</p>
     </footer>
