187afff23aff35cafd239dc930f4f7b76f07022c
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-04-30 21:15:22 -0300

    created the createRequest page

---
+++ frontend/src/pages/CreateRequest/CreateRequest.jsx
@@ -0,0 +1,10 @@
+// CSS
+import styles from "../CreateRequest.module.css"
+
+const CreateRequest = () => {
+  return (
+    <div>CreateRequest</div>
+  )
+}
+
+export default CreateRequest
--- frontend/src/utils/Router.jsx
+++ frontend/src/utils/Router.jsx
@@ -9,6 +9,7 @@ import Home from "../pages/Home/Home"
 import Cart from "../pages/Cart/Cart"
 import Login from "../pages/Login/Login"
 import Register from "../pages/Register/Register"
+import CreateRequest from "../pages/CreateRequest/CreateRequest"
 
 const Router = () => {
   return (
@@ -20,6 +21,7 @@ const Router = () => {
                 <Route path="/my-cart" element={<Cart />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/create-request" element={<CreateRequest />} />
             </Routes>
         </BrowserRouter>
     </>
