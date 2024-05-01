c705749e5b95857b0b2df34d6dd414380eaac3cf
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-01 13:41:32 -0300

    added the getRequest function

--- frontend/src/pages/RequestByUser/RequestByUser.jsx
+++ frontend/src/pages/RequestByUser/RequestByUser.jsx
@@ -1,12 +1,35 @@
 // CSS
 import styles from "./RequestByUser.module.css"
 
+// Modules
+import dbFetch from "../../axios/config"
+import { useState, useEffect } from "react"
+
 const RequestByUser = () => {
-  return (
-    <div>
-        <h1>RequestByUser</h1>
-    </div>
-  )
+    const [requests, setRequests] = useState([])
+
+    const getRequests = async() => {
+        const res = await dbFetch.get("/request/user")
+        setRequests(res.data)
+    }
+
+    useEffect(() => {
+        getRequests()
+    }, [])
+
+    return (
+        <div>
+            <h1>RequestByUser</h1>
+
+            <button onClick={() => console.log(items)}>TESTE</button>
+
+            {requests && (
+                requests.map((request) => (
+                    <p key={request.id}>{request.items}</p>
+                ))
+            )}
+        </div>
+    )
 }
 
 export default RequestByUser
