bc6ae80f53e09b79f9470967dcc6302f9593de0e
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-02 14:39:41 -0300

    added the user link on the navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -2,16 +2,27 @@
 import styles from "./Navbar.module.css"
 
 // Modules
+import { useContext } from "react"
 import { Link } from "react-router-dom"
 
+// Context
+import { UserContext } from "../../context/UserContext"
+
 const Navbar = () => {
+    const { userId } = useContext(UserContext)
+
     return (
         <nav className={styles.navbar}>
             <div className={styles.search}>PESQ</div>
             <div><Link to="/"><img src="https://via.placeholder.com/50x50" alt="Logo do Site" /></Link></div>
             <div className={styles.menu}>
-                <Link to="/my-cart">CA</Link> 
-                <Link to="/login">US</Link>
+                <Link to="/my-cart">CA</Link>
+
+                {userId != 0 ? (
+                    <Link to="/user">US</Link>
+                ) : (
+                    <Link to="/login">US</Link>
+                )}
             </div>
         </nav>
     )
