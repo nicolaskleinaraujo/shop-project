f2c4513e6cfe2927beaade0b95f7c738ec81c4bc
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-02 14:50:26 -0300

    added isAdmin function on the user page

--- frontend/src/pages/User/User.jsx
+++ frontend/src/pages/User/User.jsx
@@ -1,12 +1,35 @@
 // CSS
 import styles from "./User.module.css"
 
+// Modules
+import dbFetch from "../../axios/config"
+import { useEffect, useState, useContext } from "react"
+import { Link } from "react-router-dom"
+
+// Context
+import { UserContext } from '../../context/UserContext'
+
 const User = () => {
-  return (
-    <div>
-        <h1>User</h1>
-    </div>
-  )
+    const { userId } = useContext(UserContext)
+    const [admin, setAdmin] = useState(false)
+
+    const isAdmin = async() => {
+        const res = await dbFetch.get(`/user/${userId}`)
+        setAdmin(res.data.isAdmin)
+    }
+
+    useEffect(() => {
+        isAdmin()
+    }, [])
+
+    return (
+        <div>
+            <h1>Perfil</h1>
+            <Link to="/requests">Pedidos</Link>
+            <Link to="/infos">Mudar Informações</Link>
+            { admin && <Link to="/admin">Página de Admin</Link> }
+        </div>
+    )
 }
 
 export default User
