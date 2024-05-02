d20f3ed4c23864897ba5bcf5a4b0343c0e4dc05b
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-02 11:15:25 -0300

    removed the adminAuth on the get item by id route

--- backend/src/Routes/items.js
+++ backend/src/Routes/items.js
@@ -6,7 +6,7 @@ const adminAuth = require("../middlewares/adminAuth")
 // Routes
 router.route("/item/create").post(adminAuth, (req, res) => itemController.create(req, res))
 router.route("/item/items").get((req, res) => itemController.getAll(req, res))
-router.route("/item/:id").get(adminAuth, (req, res) => itemController.getById(req, res))
+router.route("/item/:id").get((req, res) => itemController.getById(req, res))
 router.route("/item/update").post(adminAuth, (req, res) => itemController.update(req, res))
 router.route("/item/:id").delete(adminAuth, (req, res) => itemController.delete(req, res))
 
