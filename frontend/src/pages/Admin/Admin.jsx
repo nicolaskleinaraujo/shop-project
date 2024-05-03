4c24d0d82aed0cb2d53a4c00087368560ac5d26d
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-03 19:51:12 -0300

    added all the links on the admin page

--- frontend/src/pages/Admin/Admin.jsx
+++ frontend/src/pages/Admin/Admin.jsx
@@ -1,10 +1,17 @@
 // CSS
 import styles from "./Admin.module.css"
 
+// Modules
+import { Link } from "react-router-dom"
+
 const Admin = () => {
   return (
     <div>
         <h1>Admin</h1>
+
+        <Link to="/all-users">Ver Usuarios</Link>
+        <Link to="/all-requests">Ver Pedidos</Link>
+        <Link to="/create-item">Items</Link>
     </div>
   )
 }
