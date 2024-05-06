40e08361eaee69b8030171ff2771644bea73c4eb
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-06 19:01:29 -0300

    added the setAdmin function on the app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,16 +7,20 @@ import { useLayoutEffect, useContext } from 'react'
 
 // Context
 import { UserContext } from './context/UserContext'
+import { AdminContext } from "./context/AdminContext"
 
 // Router
 import Router from './utils/Router'
 
 function App() {
-  const { setUserId } = useContext(UserContext)
+  const { userId, setUserId } = useContext(UserContext)
+  const { setAdmin } = useContext(AdminContext)
 
   const tryAuth = async() => {
     const res = await dbFetch.get("/user/tryauth")
     setUserId(res.data.id)
+    const admin = await dbFetch.get(`/user/${userId}`)
+    setAdmin(admin.data.isAdmin)
   }
 
   useLayoutEffect(() => {
