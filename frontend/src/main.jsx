ecb1cddc4f841431fccde85cc7c6ca168e3469eb
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-06 18:59:09 -0300

    added the adminProvider on the main file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,11 +5,14 @@ import './index.css'
 
 // Context
 import { UserProvider } from './context/UserContext.jsx'
+import { AdminProvider } from './context/AdminContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <AdminProvider>
     <UserProvider>
       <App />
     </UserProvider>
+    </AdminProvider>
   </React.StrictMode>,
 )
