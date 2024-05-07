43ab4a3a3a87a726408310568617fc5ac77cb004
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-06 22:29:23 -0300

    added the all users map

--- frontend/src/pages/AllUsers/AllUsers.jsx
+++ frontend/src/pages/AllUsers/AllUsers.jsx
@@ -4,6 +4,7 @@ import styles from "./AllUsers.module.css"
 // Modules
 import dbFetch from "../../axios/config"
 import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 
 const AllUsers = () => {
     const [users, setUsers] = useState([])
@@ -20,6 +21,15 @@ const AllUsers = () => {
     return (
         <div>
             <h1>All Users</h1>
+
+            {users && (
+                users.map((user) => (
+                    <div>
+                        <p>{user.fullName}</p>
+                        <button>Deletar</button>
+                    </div>
+                ))
+            )}
         </div>
     )
 }
