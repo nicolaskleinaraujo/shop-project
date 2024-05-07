8a563bd8b48630d7fa9746619ba0ed0907b87e6d
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-06 22:45:36 -0300

    added deleteAccount function

--- frontend/src/pages/AllUsers/AllUsers.jsx
+++ frontend/src/pages/AllUsers/AllUsers.jsx
@@ -6,16 +6,25 @@ import dbFetch from "../../axios/config"
 import { useState, useEffect } from "react"
 
 const AllUsers = () => {
+    const [loading, setLoading] = useState(true)
     const [users, setUsers] = useState([])
 
     const getAllUsers = async() => {
         const res = await dbFetch.get("/user")
         setUsers(res.data)
+        if (loading) {setLoading(false)}
+    }
+
+    const deleteAccount = async(id) => {
+        if (confirm("Deseja mesmo deletar? AÇÃO IRREVERSIVEL!")) {
+            await dbFetch.delete(`/user/${id}`)
+            setLoading(true)
+        }
     }
 
     useEffect(() => {
         getAllUsers()
-    }, [])
+    }, [loading])
 
     return (
         <div className={styles.all_users}>
@@ -26,7 +35,7 @@ const AllUsers = () => {
                     <div key={user.id}>
                         <p>{user.fullName}</p>
 
-                        <button>Deletar</button>
+                        <button onClick={() => deleteAccount(user.id)}>Deletar</button>
 
                         {!user.isAdmin ? (
                             <button>Tornar Admin</button>
