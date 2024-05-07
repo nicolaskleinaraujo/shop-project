9a4239ffb3d05fa7fb5ee8e739f86f36f1a548d4
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-07 11:16:08 -0300

    created the allRequests page

---
+++ frontend/src/pages/AllRequests/AllRequests.jsx
@@ -0,0 +1,12 @@
+// CSS
+import styles from "./AllRequests.module.css"
+
+const AllRequests = () => {
+  return (
+    <div>
+        <h1>All Requests</h1>
+    </div>
+  )
+}
+
+export default AllRequests
