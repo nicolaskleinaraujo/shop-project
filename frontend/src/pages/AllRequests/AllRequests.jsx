8420044a5274ff9e153817993702ebe503ec560f
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-07 11:19:57 -0300

    created the getAllRequests function

--- frontend/src/pages/AllRequests/AllRequests.jsx
+++ frontend/src/pages/AllRequests/AllRequests.jsx
@@ -1,12 +1,27 @@
 // CSS
 import styles from "./AllRequests.module.css"
 
+// Modules
+import dbFetch from "../../axios/config"
+import { useState, useEffect } from "react"
+
 const AllRequests = () => {
-  return (
-    <div>
-        <h1>All Requests</h1>
-    </div>
-  )
+    const [requests, setRequests] = useState([])
+
+    const getAllRequests = async() => {
+        const res = await dbFetch.get("/request")
+        setRequests(res.data)
+    }
+    
+    useEffect(() => {
+        getAllRequests()
+    }, [])
+
+    return (
+        <div>
+            <h1>All Requests</h1>
+        </div>
+    )
 }
 
 export default AllRequests
