5feb42e579e7b99da731c6370eb1b7f7101bd20c
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-06 22:25:24 -0300

    created the getAllUses function on allUsers page

--- frontend/src/pages/AllUsers/AllUsers.jsx
+++ frontend/src/pages/AllUsers/AllUsers.jsx
@@ -1,12 +1,27 @@
 // CSS
 import styles from "./AllUsers.module.css"
 
+// Modules
+import dbFetch from "../../axios/config"
+import { useState, useEffect } from "react"
+
 const AllUsers = () => {
-  return (
-    <div>
-        <h1>All Users</h1>
-    </div>
-  )
+    const [users, setUsers] = useState([])
+
+    const getAllUsers = async() => {
+        const res = await dbFetch.get("/user")
+        setUsers(res.data)
+    }
+
+    useEffect(() => {
+        getAllUsers()
+    }, [])
+
+    return (
+        <div>
+            <h1>All Users</h1>
+        </div>
+    )
 }
 
 export default AllUsers
