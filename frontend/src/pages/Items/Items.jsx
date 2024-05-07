be30369578bfc7f9584dd136563af12f64e53c6b
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-07 15:31:46 -0300

    created the getItems function

--- frontend/src/pages/Items/Items.jsx
+++ frontend/src/pages/Items/Items.jsx
@@ -1,12 +1,37 @@
 // CSS
 import styles from "./Items.module.css"
 
+// Modules
+import dbFetch from "../../axios/config"
+import { useState, useEffect } from "react"
+
 const Items = () => {
-  return (
-    <div>
-        <h1>Items</h1>
-    </div>
-  )
+    const [items, setItems] = useState([])
+
+    const getItems = async() => {
+        const res = await dbFetch.get("/item/items")
+        setItems(res.data)
+    }
+
+    useEffect(() => {
+        getItems()
+    }, [])
+
+    return (
+        <div>
+            <h1>Items</h1>
+
+            { items &&
+                items.map((item) => (
+                    <div key={item.id}>
+                        <p>{item.name} | {item.value}</p>
+                        <button>Atualizar</button>
+                        <button>Deletar</button>
+                    </div>
+                ))
+            }
+        </div>
+    )
 }
 
 export default Items
