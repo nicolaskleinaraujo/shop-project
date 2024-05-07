eccf1b0942ace7c15b1b90d4c285f518cc5cefd4
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-07 15:49:48 -0300

    added all the form of the updateItem page

--- frontend/src/pages/UpdateItem/UpdateItem.jsx
+++ frontend/src/pages/UpdateItem/UpdateItem.jsx
@@ -8,11 +8,15 @@ import { useParams } from "react-router-dom"
 
 const UpdateItem = () => {
     const { id } = useParams()
-    const [item, setItem] = useState([])
+    const [name, setName] = useState("")
+    const [description, setDescription] = useState("")
+    const [value, setValue] = useState()
 
     const getItem = async() => {
         const res = await dbFetch.get(`/item/${id}`)
-        setItem(res.data)
+        setName(res.data.name)
+        setDescription(res.data.description)
+        setValue(res.data.value)
     }
 
     useEffect(() => {
@@ -21,7 +25,32 @@ const UpdateItem = () => {
 
     return (
         <div>
-            <h1>Update Item</h1>
+            <form>
+                <h1>Update Item</h1>
+
+                <input 
+                    type="text" 
+                    placeholder="Nome do Produto" 
+                    onChange={(e) => setName(e.target.value)} 
+                    value={name} 
+                />
+
+                <input 
+                    type="text" 
+                    placeholder="Descrição do Produto" 
+                    onChange={(e) => setDescription(e.target.value)} 
+                    value={description} 
+                />
+
+                <input 
+                    type="number" 
+                    placeholder="Valor do Produto" 
+                    onChange={(e) => setValue(e.target.value)} 
+                    value={value} 
+                />
+
+                <input type="submit" value="Atualizar" />
+            </form>
         </div>
     )
 }
