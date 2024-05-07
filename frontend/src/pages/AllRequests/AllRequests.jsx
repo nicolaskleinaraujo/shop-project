2668d43dc0f73fe3efc3ea6f76fc2fd1fa508823
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-07 11:29:40 -0300

    added the requests visualization

--- frontend/src/pages/AllRequests/AllRequests.jsx
+++ frontend/src/pages/AllRequests/AllRequests.jsx
@@ -4,6 +4,7 @@ import styles from "./AllRequests.module.css"
 // Modules
 import dbFetch from "../../axios/config"
 import { useState, useEffect } from "react"
+import { Link } from "react-router-dom"
 
 const AllRequests = () => {
     const [requests, setRequests] = useState([])
@@ -20,6 +21,22 @@ const AllRequests = () => {
     return (
         <div>
             <h1>All Requests</h1>
+
+            { requests && 
+                requests.map((request) => (
+                    <div key={request.id}>
+                        <p>{request.items} | R${request.value}</p>
+
+                        { request.details && <p>OBS: {request.details}</p> }
+
+                        <p>
+                            <Link to={`/request/${request.slug}`}>Ver Pedido</Link> | 
+                            <button>Cancelar</button>
+                            <button>Entregue</button>
+                        </p>
+                    </div>
+                ))
+            }
         </div>
     )
 }
