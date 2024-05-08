998110484af35249505e4d3a04b1d0a77ef4099c
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-08 11:02:14 -0300

    added setSearch on the navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -4,18 +4,22 @@ import styles from "./Navbar.module.css"
 // Modules
 import { useContext } from "react"
 import { Link } from "react-router-dom"
+import { FaUser, FaShoppingCart, FaSearch } from "react-icons/fa"
 
 // Context
 import { UserContext } from "../../context/UserContext"
-import { FaUser, FaShoppingCart, FaSearch } from "react-icons/fa"
+import { SearchContext } from "../../context/SearchContext"
 
 const Navbar = () => {
     const { userId } = useContext(UserContext)
+    const { search, setSearch } = useContext(SearchContext)
 
     return (
         <nav className={styles.navbar}>
-            <div className={styles.search}><FaSearch /></div>
+            <div className={styles.search} onClick={() => setSearch(!search)}><FaSearch /></div>
+
             <div><Link to="/"><img src="https://via.placeholder.com/50x50" alt="Logo do Site" /></Link></div>
+            
             <div className={styles.menu}>
                 <Link to="/my-cart"><FaShoppingCart /></Link>
 
@@ -24,6 +28,8 @@ const Navbar = () => {
                 ) : (
                     <Link to="/login"><FaUser /></Link>
                 )}
+
+                <button onClick={() => console.log(search)}>TESTE</button>
             </div>
         </nav>
     )
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,15 +6,15 @@ import './index.css'
 // Context
 import { UserProvider } from './context/UserContext.jsx'
 import { AdminProvider } from './context/AdminContext.jsx'
-import { SearchContext } from './context/SearchContext.jsx'
+import { SearchProvider } from './context/SearchContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AdminProvider>
     <UserProvider>
-    <SearchContext>
+    <SearchProvider>
       <App />
-    </SearchContext>
+    </SearchProvider>
     </UserProvider>
     </AdminProvider>
   </React.StrictMode>,
