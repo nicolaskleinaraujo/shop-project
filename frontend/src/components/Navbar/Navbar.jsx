49cb89f2c138ab8c7c7f06acad99923195b37782
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-08 12:51:06 -0300

    hotfix on the search bar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import styles from "./Navbar.module.css"
 
 // Modules
 import { useContext } from "react"
-import { Link } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom"
 import { FaUser, FaShoppingCart, FaSearch } from "react-icons/fa"
 
 // Context
@@ -11,12 +11,29 @@ import { UserContext } from "../../context/UserContext"
 import { SearchContext } from "../../context/SearchContext"
 
 const Navbar = () => {
+    const location = useLocation()
+    const navigate = useNavigate()
+    
     const { userId } = useContext(UserContext)
     const { search, setSearch } = useContext(SearchContext)
 
     return (
         <nav className={styles.navbar}>
-            <div className={styles.search} onClick={() => setSearch(!search)}><FaSearch /></div>
+            {location.pathname == "/" ? (
+
+                <div
+                    className={styles.search} 
+                    onClick={() => setSearch(!search)} 
+                ><FaSearch /></div>
+
+            ) : (
+
+                <div
+                    className={styles.search} 
+                    onClick={() => { navigate("/"), setSearch(true) }} 
+                ><FaSearch /></div>
+
+            )}
 
             <div><Link to="/"><img src="https://via.placeholder.com/50x50" alt="Logo do Site" /></Link></div>
             
