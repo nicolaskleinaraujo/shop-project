7743f8d8d1d5f5d7b8271009487deb024ff750d9
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-08 21:51:51 -0300

    added a loading to the createItem page

--- frontend/src/pages/CreateItem/CreateItem.jsx
+++ frontend/src/pages/CreateItem/CreateItem.jsx
@@ -6,11 +6,15 @@ import dbFetch from "../../axios/config"
 import { useState } from "react"
 
 const CreateItem = () => {
+    const [loading, setLoading] = useState(false)
+    
     const [name, setName] = useState("")
     const [description, setDescription] = useState("")
     const [value, setValue] = useState()
 
     const handleCreate = async() => {
+        setLoading(true)
+
         await dbFetch.post("/item/create", {
             name,
             description,
@@ -23,30 +27,36 @@ const CreateItem = () => {
             <form onSubmit={handleCreate}>
                 <h1>Criar Item</h1>
 
-                <input 
-                    type="text" 
-                    placeholder="Nome do Produto" 
-                    onChange={(e) => setName(e.target.value)} 
-                    value={name} 
-                />
+                {loading ? (
+                    <img src="/loading.svg" alt="Carregando" />
+                ) : (
+                    <>
+                        <input 
+                            type="text" 
+                            placeholder="Nome do Produto" 
+                            onChange={(e) => setName(e.target.value)} 
+                            value={name} 
+                        />
 
-                <input 
-                    type="text" 
-                    placeholder="Descrição do Produto" 
-                    onChange={(e) => setDescription(e.target.value)} 
-                    value={description} 
-                />
+                        <input 
+                            type="text" 
+                            placeholder="Descrição do Produto" 
+                            onChange={(e) => setDescription(e.target.value)} 
+                            value={description} 
+                        />
 
-                <input 
-                    type="number" 
-                    placeholder="Valor do Produto" 
-                    onChange={(e) => setValue(e.target.value)} 
-                    value={value} 
-                    step={0.01} 
-                    min={0} 
-                />
+                        <input 
+                            type="number" 
+                            placeholder="Valor do Produto" 
+                            onChange={(e) => setValue(e.target.value)} 
+                            value={value} 
+                            step={0.01} 
+                            min={0} 
+                        />
 
-                <input type="submit" value="Criar" />
+                        <input type="submit" value="Criar" />
+                    </>
+                )}
             </form>
             
         </div>
