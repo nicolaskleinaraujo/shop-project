7daed071524c6559ead4ae9ada2700fd7c9c0400
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-14 12:41:44 -0300

    changed the cors origin url

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,7 +14,7 @@ app.use(cookieParser(process.env.COOKIE_PARSER_SECRET))
 app.use(express.json())
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://nk-shop-project.netlify.app",
     credentials: true,
   })
 )
