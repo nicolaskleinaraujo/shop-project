c64e044eaca4f97b9b2183177dbed5c405618e60
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-14 17:27:28 -0300

    added the api url to the axios baseUrl

--- frontend/src/axios/config.js
+++ frontend/src/axios/config.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 axios.defaults.withCredentials = true
 const dbFetch = axios.create({
-    baseURL: "http://localhost:3000"
+    baseURL: "https://shop-project-hwt4.onrender.com",
 })
 
 export default dbFetch
