f960f888ca4d2b69f8366acf2bafd777a940fc3d
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-15 11:25:00 -0300

    created the basic component structure

--- frontend/src/components/Box/Box.jsx
+++ frontend/src/components/Box/Box.jsx
@@ -1,10 +1,21 @@
 // CSS
 import styles from "./Box.module.css"
 
-const Box = () => {
+// Modules
+import { Link } from "react-router-dom"
+
+const Box = ({ props }) => {
   return (
-    <div>
-        <h1>Box</h1>
+    <div className={styles.box}>
+        {props.title && <p>{props.title}</p>}
+
+        {props.price && <p>{props.price}</p>}
+
+        {props.description && <p>{props.description}</p>}
+
+        {props.btnAction && <button onClick={btnAction}>{props.btnText}</button>}
+
+        {props.link && <Link to={props.link}>{props.linkText}</Link>}
     </div>
   )
 }
