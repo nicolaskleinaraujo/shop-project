9c8c73a52f6a9a3a24abcd8f73f8ade05f7a1309
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-15 12:09:59 -0300

    hotfixed the box structure

--- frontend/src/components/Box/Box.jsx
+++ frontend/src/components/Box/Box.jsx
@@ -4,16 +4,16 @@ import styles from "./Box.module.css"
 // Modules
 import { Link } from "react-router-dom"
 
-const Box = ({ props }) => {
+const Box = (props) => {
     return (
         <div className={styles.box}>
             {props.title && <p className={styles.box_title}>{props.title}</p>}
 
-            {props.price && <p className={styles.box_price}>{props.price}</p>}
+            {props.value && <p className={styles.box_value}>{props.value}</p>}
 
             {props.desc && <p className={styles.box_desc}>{props.desc}</p>}
 
-            {props.btnAction && <button onClick={btnAction} className={styles.box_btn}>{props.btnText}</button>}
+            {props.btnText && <button onClick={() => btnAction} className={styles.box_btn}>{props.btnText}</button>}
 
             {props.link && <Link to={props.link} className={styles.box_link}>{props.linkText}</Link>}
         </div>
