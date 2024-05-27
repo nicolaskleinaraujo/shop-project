5f652986ed21dfb9e054919c5fbd1e629ceaf399
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-27 16:42:18 -0300

    change the user test file name to create user test

---
+++ backend/src/tests/user/createUser.test.js
@@ -0,0 +1,36 @@
+// Modules
+const supertest = require("supertest")
+const request = supertest("http://localhost:3000")
+
+// Tests
+describe("Create account routes", () => {
+    const payload = {
+        fullName: "Nicolas Klein Araujo",
+        email: "nicolas@gmail.com",
+        number: 123456789,
+        password: "12345",
+        city: "Maringá",
+        street: "Av. Juscelino Kubitschek",
+        houseNum: 258,
+    }
+
+    it("Should create a account", async() => {
+        const res = await request.post("/user/create").send(payload)
+        expect(res.statusCode).toBe(200)
+    })
+
+    it("Should return a missing info message", async() => {
+        const res = await request.post("/user/create").send(payload.fullName)
+        
+        expect(res.body.msg).toBe("Informações insuficientes")
+        expect(res.statusCode).toBe(400)
+    })
+
+    it("Should return a email already cadastered message", async() => {
+        await request.post("/user/create").send(payload)
+        const res = await request.post("/user/create").send(payload)
+
+        expect(res.body.msg).toBe("Email ou numero já cadastrado")
+        expect(res.statusCode).toBe(400)
+    })
+})
