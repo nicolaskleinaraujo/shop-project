226bfa5ae8bb7b6c3f7f2092342d45571a60ac38
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-27 10:17:08 -0300

    created the create account test with jest

--- backend/src/tests/user.test.js
+++ backend/src/tests/user.test.js
@@ -1,6 +1,10 @@
+// Modules
 const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
+const supertest = require("supertest")
+const request = supertest("http://localhost:3000")
 
+// Setup
 beforeAll(async() => {
     prisma.$connect()
 })
@@ -13,4 +17,21 @@ beforeEach(async() => {
 
 afterAll(async() => {
     prisma.$disconnect()
+
+// Tests
+describe("Create account routes", () => {
+    it("Should create a account", async() => {
+        const payload = {
+            fullName: "Nicolas Klein Araujo",
+            email: "nicolas@gmail.com",
+            number: 123456789,
+            password: "12345",
+            city: "Maringá",
+            street: "Av. Juscelino Kubitschek",
+            houseNum: 258,
+        }
+
+        const res = await request.post("/user/create").send(payload)
+        expect(res.statusCode).toBe(200)
+    })
 })
