285b01df6f580e6d9506755b46815c756ea5b341
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-05-28 00:28:27 -0300

    add a updateUser test file

---
+++ backend/src/tests/user/updateUser.test.js
@@ -0,0 +1,78 @@
+// Modules
+const supertest = require("supertest")
+const request = supertest("http://localhost:3000")
+
+// Tests
+describe("Update account route", () => {
+    const payload = {
+        fullName: "Nicolas Klein Araujo",
+        email: "nicolas@gmail.com",
+        number: 123456789,
+        password: "12345",
+        city: "Maringá",
+        street: "Av. Juscelino Kubitschek",
+        houseNum: 258,
+    }
+
+    const updatePayload = {
+        fullName: "test",
+        email: "test@gmail.com",
+        number: 123,
+        password: "12345",
+        city: "test",
+        street: "test",
+        houseNum: 258,
+    }
+
+    it("Should update the account infos", async() => {
+        const userCredentials = await request.post("/user/create").send(updatePayload)
+        updatePayload.id = userCredentials.body.id
+        
+        const res = await request.post("/user/update").send(updatePayload).set("Cookie", userCredentials.headers['set-cookie'])
+        expect(res.statusCode).toBe(200)
+
+        delete updatePayload.id
+    })
+
+    it("Should return a missing info message", async() => {
+        const userCredentials = await request.post("/user/create").send(updatePayload)
+        updatePayload.id = userCredentials.body.id
+
+        const res = await request.post("/user/update").send(updatePayload.fullName).set("Cookie", userCredentials.headers['set-cookie'])
+        expect(res.body.msg).toBe("Informações insuficientes")
+        expect(res.statusCode).toBe(400)
+
+        delete updatePayload.id
+    })
+
+
+    it("Should return a email already cadastered message", async() => {
+        await request.post("/user/create").send(payload)
+
+        const userCredentials = await request.post("/user/create").send(updatePayload)
+        updatePayload.id = userCredentials.body.id
+        updatePayload.email = payload.email
+
+        const res = await request.post("/user/update").send(updatePayload).set("Cookie", userCredentials.headers['set-cookie'])
+        expect(res.body.msg).toBe("Email já cadastrado")
+        expect(res.statusCode).toBe(400)
+
+        delete updatePayload.id
+        updatePayload.email = "test@gmail.com"
+    })
+
+    it("Should return a number already cadastered message", async() => {
+        await request.post("/user/create").send(payload)
+
+        const userCredentials = await request.post("/user/create").send(updatePayload)
+        updatePayload.id = userCredentials.body.id
+        updatePayload.number = payload.number
+
+        const res = await request.post("/user/update").send(updatePayload).set("Cookie", userCredentials.headers['set-cookie'])
+        expect(res.body.msg).toBe("Numero já cadastrado")
+        expect(res.statusCode).toBe(400)
+
+        delete updatePayload.id
+        updatePayload.number = 123
+    })
+})
