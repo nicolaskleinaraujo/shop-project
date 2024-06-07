7f773d01671544398788945b9d9adc295fe4b3df
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-06-07 17:23:43 -0300

    created the login user tests file

---
+++ backend/src/tests/user/login.test.js
@@ -0,0 +1,22 @@
+// Modules
+const app = require("../../index")
+const request = require("supertest")(app)
+
+// Setup
+let data = {}
+
+beforeEach(() => {
+    data = {
+        fullName: "Nicolas Klein Araujo",
+        email: `${Date.now()}@gmail.com`,
+        number: parseInt(Date.now().toString().slice(-9)),
+        password: "12345",
+        city: "Maringá",
+        street: "Av. Juscelino Kubitschek",
+        houseNum: 258,
+    }
+})
+
+// Tests
+describe("Login account route", () => {
+})
