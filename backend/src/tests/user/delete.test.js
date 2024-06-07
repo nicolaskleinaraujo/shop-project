32580b1bc7c8773d26ae08f013e6eb78a92b0ae1
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-06-07 17:05:42 -0300

    create the id not specified msg test

--- backend/src/tests/user/delete.test.js
+++ backend/src/tests/user/delete.test.js
@@ -1,7 +1,6 @@
 // Modules
 const app = require("../../index")
 const request = require("supertest")(app)
-const clearDatabase = require("../../db/clearDatabase")
 
 // Setup
 let data = {}
@@ -21,13 +20,20 @@ beforeEach(() => {
 // Tests
 describe("Delete account route", () => {
     it("Should delete a account", async() => {
-        await clearDatabase()
-
         const credentials = await request.post("/user/create").send(data)
         const cookie = credentials.headers['set-cookie']
 
         const res = await request.delete(`/user/${credentials.body.id}`).set("Cookie", cookie)
         expect(res.statusCode).toBe(200)
         expect(res.body.msg).toBe("Usuario deletado com sucesso")
+    })
+
+    it("Should return ID not specified message", async() => {
+        const credentials = await request.post("/user/create").send(data)
+        const cookie = credentials.headers['set-cookie']
+        
+        const res = await request.delete(`/user/notAnId`).set("Cookie", cookie)
+        expect(res.statusCode).toBe(400)
+        expect(res.body.msg).toBe("ID não especificado")
     })
 })
