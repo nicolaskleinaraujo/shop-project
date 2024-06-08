67b1d191f9d790169d83876c20174853ffbf7a75
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-06-08 20:41:33 -0300

    create the update request tests file

---
+++ backend/src/tests/request/update.test.js
@@ -0,0 +1,30 @@
+// Modules
+const app = require("../../index")
+const request = require("supertest")(app)
+
+// Setup
+let requestData = {}
+let userData = {}
+
+beforeEach(() => {
+    requestData = {
+        items: "Cheesecake, Pancake",
+        details: "Lactose-free pancake",
+        value: 15.99,
+    }
+
+    userData = {
+        fullName: "Admin User",
+        email: "admin@gmail.com",
+        number: 123456789,
+        password: "12345",
+        city: "Admin City",
+        street: "Admin Street",
+        houseNum: 258,
+    }
+})
+
+// Tests
+describe("Create request route", () => {
+    
+})
