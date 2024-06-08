abf3bc4702c61437c7dde98924fcf653b44dce08
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-06-08 18:15:41 -0300

    create the delete item tests file

---
+++ backend/src/tests/item/delete.test.js
@@ -0,0 +1,31 @@
+// Modules
+const app = require("../../index")
+const request = require("supertest")(app)
+const prisma = require("../../db/client")
+
+// Setup
+let itemData = {}
+let userData = {}
+
+beforeEach(() => {
+    itemData = {
+        name: "Cheesecake",
+        description: "Cheesecake with red fruit syrup",
+        value: 9.99,
+    }
+
+    userData = {
+        fullName: "Admin User",
+        email: "admin@gmail.com",
+        number: 123456789,
+        password: "12345",
+        city: "Admin City",
+        street: "Admin Street",
+        houseNum: 258,
+    }
+})
+
+// Tests
+describe("Delete item route", () => {
+    
+})
