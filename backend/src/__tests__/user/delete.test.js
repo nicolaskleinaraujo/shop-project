2abe69c286c65858925694b24c9daaa9e0944ddd
Nicolas Klein <141509949+nicolaskleinaraujo@users.noreply.github.com>
2024-06-11 16:53:31 -0300

    refactor the delete user tests file

--- backend/src/__tests__/user/delete.test.js
+++ backend/src/__tests__/user/delete.test.js
@@ -3,10 +3,11 @@ const app = require("../../app")
 const request = require("supertest")(app)
 
 // Setup
-let data = {}
+let userData
+let cookie
 
-beforeEach(() => {
-    data = {
+beforeEach(async() => {
+    userData = {
         fullName: "Nicolas Klein Araujo",
         email: `${Date.now()}@gmail.com`,
         number: parseInt(Date.now().toString().slice(-9)),
@@ -15,34 +16,30 @@ beforeEach(() => {
         street: "Av. Juscelino Kubitschek",
         houseNum: 258,
     }
+
+    // Creating new user and assigning values to the payload
+    const userCredentials = await request.post("/user/create").send(userData)
+    userData.id = userCredentials.body.id
+    cookie = userCredentials.headers['set-cookie']
 })
 
 // Tests
 describe("Delete account route", () => {
     it("Should delete a account", async() => {
-        const credentials = await request.post("/user/create").send(data)
-        const cookie = credentials.headers['set-cookie']
-
-        const res = await request.delete(`/user/${credentials.body.id}`).set("Cookie", cookie)
-        expect(res.statusCode).toBe(200)
-        expect(res.body.msg).toBe("Usuario deletado com sucesso")
+        const test = await request.delete(`/user/${userData.id}`).set("Cookie", cookie)
+        expect(test.statusCode).toBe(200)
+        expect(test.body.msg).toBe("Usuario deletado com sucesso")
     })
 
     it("Should return ID not specified message", async() => {
-        const credentials = await request.post("/user/create").send(data)
-        const cookie = credentials.headers['set-cookie']
-        
-        const res = await request.delete(`/user/notAnId`).set("Cookie", cookie)
-        expect(res.statusCode).toBe(400)
-        expect(res.body.msg).toBe("ID não especificado")
+        const test = await request.delete(`/user/notAnId`).set("Cookie", cookie)
+        expect(test.statusCode).toBe(400)
+        expect(test.body.msg).toBe("ID não especificado")
     })
 
     it("Should return a user doesn't exist message", async() => {
-        const credentials = await request.post("/user/create").send(data)
-        const cookie = credentials.headers['set-cookie']
-        
-        const res = await request.delete(`/user/${credentials.body.id + 1}`).set("Cookie", cookie)
-        expect(res.statusCode).toBe(400)
-        expect(res.body.msg).toBe("Usuario inexistente")
+        const test = await request.delete(`/user/${userData.id + 1}`).set("Cookie", cookie)
+        expect(test.statusCode).toBe(400)
+        expect(test.body.msg).toBe("Usuario inexistente")
     })
 })
